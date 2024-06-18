ad19169bcf284ab4cbec93e78c58ac35acd6437c
OBEY <obeyblessing@gmail.com>
2024-06-18 14:58:47 +0200

    just before starting part4

--- src/main/java/dev/obey/employee_registration/controller/EmployeeController.java
+++ src/main/java/dev/obey/employee_registration/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package dev.obey.employee_registration.controller;
 
+import dev.obey.employee_registration.domain.Employee;
 import dev.obey.employee_registration.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -19,4 +22,16 @@ public class EmployeeController {
         model.addAttribute("listEmployees", employeeService.getEmployees());
         return  "index";
     }
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+    }
+   @PostMapping("/saveEmployee")
+   public String saveEmployee(@ModelAttribute ("employee") Employee employee){
+                employeeService.saveEmployee(employee);
+                return  "redirect:/";
+    }
+
 }
