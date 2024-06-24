a53f1d7d3f39a3be6b64076adfab05b1c59ac752
OBEY <obeyblessing@gmail.com>
2024-06-24 17:23:17 +0200

    building image

--- src/main/java/dev/obey/employee_registration/Application.java
+++ src/main/java/dev/obey/employee_registration/Application.java
@@ -7,10 +7,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = {EmployeeRepository.class})
-public class EmployeeRegistrationAppApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeRegistrationAppApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
